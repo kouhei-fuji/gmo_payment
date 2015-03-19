6f81683e88e706f2e753a4e29db3387343c1f4af
Kouhei Fujigaya <kouhei.fujigaya@gmail.com>
2015-03-19 13:31:10 +0900

    Define GLOSSARY constants for converting the GMO terms with test code

---
+++ spec/gmo_payment/glossary_spec.rb
@@ -0,0 +1,15 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe 'GmoPayment::GLOSSARY' do
+  subject { GmoPayment::GLOSSARY }
+
+  it 'is Hash<Symbol, String>' do
+    expect(subject.keys).to all be_a(Symbol)
+    expect(subject.values).to all be_a(String)
+  end
+
+  it 'is 58 pairs' do
+    expect(subject.length).to eq(58)
+  end
+end
